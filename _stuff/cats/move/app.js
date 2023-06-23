474a81aacd3a5ca259ce13e426bc280d465ce4a3
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-06-23 17:43:18 +0200

    fix move

--- _stuff/cats/move/app.js
+++ _stuff/cats/move/app.js
@@ -1,17 +1,55 @@
 let a = [
-"Le Principal",
-"Sick of Myself",
-"Burning Days",
-"Wahou !",
-"Notre tout petit petit mariage",
-"Umami",
-"Mon père et moi",
-"L'exorciste du Vatican",
-"Jeanne de Barry",
-"L'Amour et les Forêts",
-"Dernière nuit à Milan",
-"The Boogeyman"
+  'yncvpaveC rY',
+  'syrflZ sb xpvF',
+  'flnQ tavaehO',
+  '! hbunJ',
+  'rtnvenz gvgrc gvgrc ghbg regbA',
+  'vznzH',
+  'vbz gr reèc abZ',
+  "anpvgnI hq rgfvpebkr'Y",
+  'leenO rq raanrW',
+  "fgêebS fry gr ehbzN'Y",
+  'anyvZ à gvha reèvaerQ',
+  'anzlrtbbO ruG'
 ]
+
+/*
+// console.log(a)
+let i = -1
+while (++i < a.length) {
+    let res = ''
+    let j = -1
+    let ascii
+    let ascii_A = 'A'.charCodeAt(0)
+    let ascii_Z = 'Z'.charCodeAt(0)
+    let ascii_a = 'a'.charCodeAt(0)
+    let ascii_z = 'z'.charCodeAt(0)
+    while (++j < a[i].length) {
+
+        // Upper
+        if (a[i].charCodeAt(j) >= ascii_A && a[i].charCodeAt(j) <= ascii_Z) {
+            ascii = a[i].charCodeAt(j) + 13
+            if (ascii > ascii_Z) {
+                ascii -= 26
+            }
+            res += String.fromCharCode(ascii)
+        // Lower
+        } else if (a[i].charCodeAt(j) >= ascii_a && a[i].charCodeAt(j) <= ascii_z) {
+            ascii = a[i].charCodeAt(j) + 13
+            if (ascii > ascii_z) {
+                ascii -= 26
+            }
+            res += String.fromCharCode(ascii)
+        // non alnum
+        } else {
+            res += a[i][j]
+        }
+    }
+    a[i] = res
+}
+// console.log(a)
+*/
+
 let wahou = document.getElementsByClassName('waooo')
 for (let elem of wahou) {
 elem.onclick = doSomething
@@ -29,3 +67,6 @@ elem.style.color = 'rgb(' + R + ',' + G + ',' + B + ')'
 window.onload = () => {
 doSomething()
 }
+
+
+
