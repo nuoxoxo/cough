93f6390199afe90093ab2d55f810a2c7f253cb3b
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-06-24 12:28:29 +0200

    conway on html : works fine, using requestAnimationFrame

--- _stuff/homepage_conway/japp.js
+++ _stuff/homepage_conway/japp.js
@@ -129,106 +129,107 @@
 // }
 
 
-const W = 42;
-const H = W;
+
+const W = 100;
+const H = 42;
 let G = [];
 let Charset = '@#$%&?¬πø¥†®©∑œ∂ßµ∆≤∫çƒƒ';
 let Symbol = Charset[Math.floor(Math.random() * Charset.length)];
 
 window.onload = () => {
-  document.body.style.backgroundColor = 'black';
-  let ctn = document.querySelector('.container');
-  let res = '';
-  let i = -1;
-  while (++i < H) {
-    let cells = '';
-    let row = [];
-    let j = -1;
-    while (++j < W) {
-      let chr;
-      if (Math.random() >= 0.4) {
-        chr = Symbol;
-        cells += chr;
-        row.push(chr);
-      } else {
-        chr = '.';
-        cells += chr;
-        row.push(chr);
-      }
-      if (j ^ W - 1) {
-        cells += ' ';
-      }
+    document.body.style.backgroundColor = 'green';
+    let ctn = document.querySelector('.container');
+    let res = '';
+    let i = -1;
+    while (++i < H) {
+        let cells = '';
+        let row = [];
+        let j = -1;
+        while (++j < W) {
+            let chr;
+            if (Math.random() >= 0.4) {
+                chr = Symbol;
+                cells += chr;
+                row.push(chr);
+            } else {
+                chr = '.';
+                cells += chr;
+                row.push(chr);
+            }
+            if (j ^ W - 1) {
+                cells += ' ';
+            }
+        }
+        let rowId = 'row-' + i; // ...
+        res += '<div id=\'' + rowId + '\' class=\'rows\'>' + cells + '</div>';
+        G.push(row);
     }
-    let rowId = 'row-' + i; // ... 
-    res += '<div id=\'' + rowId + '\' class=\'rows\'>' + cells + '</div>';
-    G.push(row);
-  }
-  ctn.innerHTML = res;
-  ctn.onclick = startAutomaton;
+    ctn.innerHTML = res;
+    ctn.onclick = start_automaton;
 };
 
-function startAutomaton() {
-  requestAnimationFrame(iterateAutomaton);
+function start_automaton() {
+    requestAnimationFrame(iterate_automaton);
 }
 
-function iterateAutomaton() {
-  iteration();
-  let ctn = document.querySelector('.container');
-  let i = -1;
-  while (++i < H) {
-    let rowId = 'row-' + i;
-    let row = document.getElementById(rowId);
-    row.innerText = G[i].join(' ');
-  }
-  requestAnimationFrame(iterateAutomaton);
+function iterate_automaton() {
+    iteration();
+    let ctn = document.querySelector('.container');
+    let i = -1;
+    while (++i < H) {
+        let rowId = 'row-' + i;
+        let row = document.getElementById(rowId);
+        row.innerText = G[i].join(' ');
+    }
+    requestAnimationFrame(iterate_automaton);
 }
 
 function iteration() {
-  let g = [];
-  let r = -1,
-    c;
-  while (++r < H) {
-    let tmp = [];
-    c = -1;
-    while (++c < W) {
-      let n = count_neighbors(r, c);
-      if (G[r][c] === '.') {
-        if (n === 3) {
-          tmp.push(Symbol);
-        } else {
-          tmp.push('.');
+    let g = [];
+    let r = -1,
+        c;
+    while (++r < H) {
+        let tmp = [];
+        c = -1;
+        while (++c < W) {
+            let n = count_neighbors(r, c);
+            if (G[r][c] === '.') {
+                if (n === 3) {
+                    tmp.push(Symbol);
+                } else {
+                    tmp.push('.');
+                }
+                continue;
+            }
+            if ([2, 3].includes(n)) {
+                tmp.push(Symbol);
+            } else {
+                tmp.push('.');
+            }
         }
-        continue;
-      }
-      if ([2, 3].includes(n)) {
-        tmp.push(Symbol);
-      } else {
-        tmp.push('.');
-      }
+        g.push(tmp);
     }
-    g.push(tmp);
-  }
-  G = g.map(function (a) {
-    return a.slice();
-  });
+    G = g.map(function(a) {
+        return a.slice();
+    });
 }
 
 function count_neighbors(R, C) {
-  let dir = [-1, 0, 1];
-  let res = 0,
-    r,
-    c;
-  for (let a of dir) {
-    for (let b of dir) {
-      if (a === 0 && b === 0) {
-        continue;
-      }
-      r = (R + a + H) % H;
-      c = (C + b + W) % W;
-      if (G[r][c] === Symbol) {
-        ++res;
-      }
+    let dir = [-1, 0, 1];
+    let res = 0,
+        r,
+        c;
+    for (let a of dir) {
+        for (let b of dir) {
+            if (a === 0 && b === 0) {
+                continue;
+            }
+            r = (R + a + H) % H;
+            c = (C + b + W) % W;
+            if (G[r][c] === Symbol) {
+                ++res;
+            }
+        }
     }
-  }
-  return res;
+    return res;
 }
