e22cc5aeb5c62f1de2d08289fab5069b02415449
nuo <49533950+nuoxoxo@users.noreply.github.com>
2023-07-01 19:10:21 +0200

    2201 : forEach

--- _stuff/simple_tasks/2201.ts
+++ _stuff/simple_tasks/2201.ts
@@ -3,10 +3,17 @@ import * as fs from 'fs'
 let input:string[] = fs.readFileSync('inputs/2201.in').toString().trim().split('\n\n')
 let arr:number[] = []
 
+/*
 for (let i of input) {
     let sum = i.split('\n').map((n) => parseInt(n)).reduce((a, b) => a + b, 0)
     arr.push(sum)
 }
+*/
+
+input.forEach((i) => {
+    let sum = i.split('\n').map((n) => parseInt(n)).reduce((a, b) => a + b, 0)
+    arr.push(sum)
+})
 
 arr.sort((a, b) => {return b - a})
 
